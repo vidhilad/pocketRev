98bf256075f27494d238d51de58304d62a89aa9c
Hardi09 <hardimajmundar0906@gmail.com>
2022-08-04 14:31:27 -0400

    form-validatuin improved as suggested by professor

--- js/signUp.js
+++ js/signUp.js
@@ -1,67 +1,67 @@
-function validation() {
-    let fname = document.getElementById('fname').value;
-    let lname = document.getElementById('lname').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
-    let password = document.getElementById('password').value;
-    let conformPass = document.getElementById('conformPass').value;
+const $ = (selector) => document.querySelector(selector);
+const validation=() =>{
+    const fname = $('#firstName');
+    const lname =$('#lastName');
+    const email = $('#emailId');
+    const phone = $('#phoneNumber');
+    const password = $('#pass');
+    const conformPass = $('#conPass');
     let namepattern = /^[a-zA-Z]+$/;
+    let validReturn= true;
 
-    if (fname == "") {
-        document.getElementById('firstName').innerHTML = " **Please fill the first name field";
-        return false;
+    if (fname.value == "") {
+        fname.nextElementSibling.textContent=' **Please fill the first name field';
+        validReturn=false;
     }
-    else if(!namepattern.test(fname)){
-        document.getElementById('firstName').innerHTML='Pelase enter letters only';
-        return false;
+    else if(!namepattern.test(fname.value)){
+        fname.nextElementSibling.textContent='Pelase enter letters only';
+        validReturn=false;
       }
-      
-    if (lname == "") {
-        document.getElementById('lastName').innerHTML = " **Please fill the last name field";
-        return false;
+    if (lname.value == "") {
+        lname.nextElementSibling.textContent='Please fill the last name field'
+        validReturn=false;
     }
-
-    else if(!namepattern.test(lname)){
-        document.getElementById('lastName').innerHTML='Pelase enter letters only';
-        return false;
+    else if(!namepattern.test(lname.value)){
+        lname.nextElementSibling.textContent='Pelase enter letters only'
+        validReturn=false;
       }
-    if (email == "") {
-        document.getElementById('emailId').innerHTML = " **Please fill the email field";
-        return false;
+    if (email.value == "") {
+        email.nextElementSibling.textContent='Please fill the email field';
+        validReturn=false;
     }
-
-    if (email.indexOf('@') <= 0) {
-        document.getElementById('emailId').innerHTML = " **Invalid email id";
-        return false;
+    if (email.value.indexOf('@') <= 0) {
+        email.nextElementSibling.textContent='**Invalid email id';
+        validReturn=false;
     }
-
-    if ((email.charAt(email.length - 4) != '.') && (email.charAt(email.length - 3) != '.')) {
-        document.getElementById('emailId').innerHTML = " **Invalid email id";
-        return false;
+    if ((email.value.charAt(email.value.length - 4) != '.') && (email.value.charAt(email.value.length - 3) != '.')) {
+        email.nextElementSibling.textContent='Please fill the email field';
+        validReturn=false;
     }
-    if (phone == "") {
-        document.getElementById('phoneNumber').innerHTML = " **Please fill the phone number field";
-        return false;
+    if (phone.value == "") {
+        phone.nextElementSibling.textContent=' **Please fill the phone number field';
+        validReturn=false;
     }
-
-    if (password == "") {
-        document.getElementById('pass').innerHTML = " **Please fill the password";
-        return false;
+    if (password.value === "") {
+        password.nextElementSibling.textContent='  **Please fill the password';
+        validReturn=false;
     }
-
-    if ((password.length <= 5) || (password.length > 10)) {
-        document.getElementById('pass').innerHTML = " **Password is not strong";
-        return false;
+    else if((password.value.length <= 5) || (password.value.length > 10)) {
+        password.nextElementSibling.textContent=' **Password is not strong';
+        validReturn=false;
     }
-
-    if (conformPass == "") {
-        document.getElementById('conPass').innerHTML = " **Please fill the confirm password field";
-        return false;
+    if (conformPass.value === "") {
+        conformPass.nextElementSibling.textContent=' **Please fill the confirm password field';
+        // document.getElementById('conPass').innerHTML = " **Please fill the confirm password field";
+        validReturn=false;
     }
-
-
-    if (password != conformPass) {
-        document.getElementById('conPass').innerHTML = " **Password is not matching";
-        return false;
+    if (password.value != conformPass.value) {
+        conformPass.nextElementSibling.textContent=' **Password is not matching';
+        validReturn=false;
     }   
-}
+    if(validReturn == true){
+        $('form').submit();
+    }
+};
+document.addEventListener('DOMContentLoaded', () => {
+    $('#signUp').addEventListener('click', validation);
+  });
