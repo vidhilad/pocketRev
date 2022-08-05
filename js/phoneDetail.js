cf6f4e52f4424b88c309491772f21bbd54049f5b
Hardi09 <hardimajmundar0906@gmail.com>
2022-08-05 12:55:49 -0400

    comments

--- js/phoneDetail.js
+++ js/phoneDetail.js
@@ -1,17 +1,21 @@
 const thumbs=document.querySelector(".thumb-img").children;
 let likeBtn = document.querySelector('#likeBtn');
-let disLikeBtn = document.querySelector('#disLikeBtn');
-let input1 = document.querySelector('#input1');
-let input2 = document.querySelector('#input2');
+const disLikeBtn = document.querySelector('#disLikeBtn');
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
 
+    //like btn incrementation
 likeBtn.addEventListener('click', ()=>{
   input1.value = parseInt(input1.value) + 1;
   input1.style.color = "#0000ff";
 })
+//dislike button dicrementation
 disLikeBtn.addEventListener('click', ()=>{
   input2.value = parseInt(input2.value) - 1;
   input2.style.color = "#ff0000";
 })
+
+//image change event
 function changeImage(event){
 document.querySelector(".pro-img").src=event.children[0].src
 
