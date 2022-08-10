e43d8f3e467dd0122ca3f3aa662bba11b873d470
Vidhi Lad <vidhilad2009@gmail.com>
2022-08-10 16:20:26 -0400

    Commit

---
+++ js/laptop.js
@@ -0,0 +1,7 @@
+/**
+ * Javascript for flipping the image to display about its price and name when the cursor hover's over the image.
+ */
+
+ $('.flip-card').click(function() {
+    $(this).toggleClass('hover');
+});
